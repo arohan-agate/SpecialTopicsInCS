8715537505d79d88d60d111c138edb2a94832059
Arohan Agate <arogate05@gmail.com>
2021-11-10 21:17:33 -0800

    Create LinkedListPractice.java

---
+++ LinkedList/LinkedListPractice.java
@@ -0,0 +1,16 @@
+// Problem 9:
+list.next.next = new ListNode(3);
+
+// Problem 10:
+list = new ListNode(3);
+list.next = new ListNode(1);
+list.next.next = new ListNode(2);
+
+// Problem 11:
+temp.next.next = new ListNode(2);
+list.next = temp;
+
+// Problem 12:
+list = new ListNode(2, null);
+ListNode temp = new ListNode(3, null);
+ListNode list2 = new ListNode (1, temp);
